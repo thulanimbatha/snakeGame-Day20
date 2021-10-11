bed874d9063c88848d6a75326998748300d35143
thulanimbatha <thulanimbatha06@gmail.com>
2021-10-11 22:36:11 +0200

    completed TODO

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 # Snake game
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python: The Snake Game")
+screen.tracer(0)    # show no graphics on screen
 
-# TODO Create snake body
+starting_positions = [(-20,0), (-40,0)]
+snake_segments = []
+
 head = Turtle("square")
 head.color("white")
 head.penup()
 
-body = Turtle("square")
-body.color("white")
-body.penup()
-body.goto(-20,0)
+for position in starting_positions:
+    body = Turtle(shape="square")
+    body.penup()
+    body.color("white")
+    body.goto(position)
+    snake_segments.append(body)
 
-tail = Turtle("square")
-tail.color("white")
-tail.penup()
-tail.goto(-40,0)
+# show graphics now
+screen.update()
 
-
-
+# TODO Move Snake
+while True:
+    # move snake
+    head.forward(20)
+    for segment in snake_segments:
+        segment.forward(20)
+    # show changes
+    screen.update()
+    time.sleep(0.2)
 screen.exitonclick()
