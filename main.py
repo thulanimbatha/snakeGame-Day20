2d8b9f3d20573df5c7bda015304cb5c4e05380c2
thulanimbatha <thulanimbatha06@gmail.com>
2021-10-11 21:35:27 +0200

    completed screen setup

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Snake game
+# TODO 1: screen setup
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Python: The Snake Game")
+
+screen.exitonclick()
