41d1add616dd52f1620db210835239e87b915fe8
thulanimbatha <thulanimbatha06@gmail.com>
2021-10-11 21:56:37 +0200

    completed TODO

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 # Snake game
-# TODO 1: screen setup
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python: The Snake Game")
+
+# TODO Create snake body
+head = Turtle("square")
+head.color("white")
+head.penup()
+
+body = Turtle("square")
+body.color("white")
+body.penup()
+body.goto(-20,0)
+
+tail = Turtle("square")
+tail.color("white")
+tail.penup()
+tail.goto(-40,0)
+
+
 
 screen.exitonclick()
